a2b47d9dcadbd63fad1a0c3c8b89a0fa62ebe7ae
ijf8090 <i.finlay@yahoo.com>
2020-01-19 21:00:44 -0500

    Dogs,dogs and more dogs. Platying with logic.

---
+++ ex29.py
@@ -0,0 +1,30 @@
+people = 50
+cats = 25
+dogs = 10
+
+if people < cats:
+    print("Too many cats, the world is doomed!")
+
+if people > cats:
+    print("Not many cats, the world is saved!")
+
+
+if people < dogs:
+    print("The world is drooled on")
+
+if people > dogs:
+    print("The world is dry!")
+
+dogs += 5
+
+if people != dogs:
+    print("People are not equal to dogs.")
+
+if people == dogs:
+    print("People are equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+    print("People are dogs.")
