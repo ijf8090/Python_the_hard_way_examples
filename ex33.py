1e65eb8363464ec27872912248369d0399f347f1
ijf8090 <i.finlay@yahoo.com>
2020-01-20 13:55:31 -0500

    extra commas

--- ex33.py
+++ ex33.py
@@ -2,12 +2,12 @@ i = 0
 numbers = []
 
 while i< 6:
- print("At the top i is %d", % i)
+ print("At the top i is %d" % i)
  numbers.append[i]
 
  i+=
  print("numbers now: " , numbers)
- print("At the bottom i is %d", % i)
+ print("At the bottom i is %d" % i)
 
 print("The numbers: ")
 
