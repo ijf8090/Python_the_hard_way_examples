a6ae8519e2f51aca1555beb49e2837d86137bb8d
ijf8090 <i.finlay@yahoo.com>
2020-01-20 14:18:41 -0500

    Added increment parameter

--- ex33_drill.py
+++ ex33_drill.py
@@ -1,19 +1,22 @@
 i = 0
 numbers = []
+
+increment = int(input("Enter the increment value: "))
 initial_count_value = int(input("Enter the starting value: "))
 
-def loop_function(counter,list_var):
+def loop_function(counter,list_var, increment_value):
     print("counter is %d" % counter
     )
     while counter < 6:
      print("At the top counter is %d" % counter)
      list_var.append(counter)
 
-     counter += 1
+     counter = counter + increment_value
+
      print("numbers now: " , list_var)
      print("At the bottom counter is %d" % counter)
 
-loop_function(initial_count_value,numbers)
+loop_function(initial_count_value,numbers, increment)
 
 print("The numbers: ")
 for num in numbers:
