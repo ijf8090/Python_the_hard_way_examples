65785ee5e067f8ea14776eba7753b80f74ffa2ec
ijf8090 <i.finlay@yahoo.com>
2020-01-19 21:33:00 -0500

    raw_input not supported

--- ex31.py
+++ ex31.py
@@ -1,13 +1,13 @@
 print("You enter a dark room with two doors. Do you go through door #1 \
 or door #2?")
 
-door = raw_input("> ")
+door = input("> ")
 if door == "1":
     print("Theres a giant bear here eating a cheese cake. What do you do?")
     print("1.   Take the cake.")
     print("2.   Scream at the bear.")
 
-    bear  = raw_input("> ")
+    bear  = input("> ")
 
     if bear == '1':
         print("The bear eats your face off. Good job!")
@@ -22,7 +22,7 @@ elif door == "2":
     print("2    Yellow jacket clothespins")
     print("3.   Understanding revolvers yelling melodies")
 
-    insanity  = raw_input("> ")
+    insanity  = input("> ")
 
     if insanity == '1' or insanity == '2':
         print (" Your body survives powered by a mind of jello. Good job!")
