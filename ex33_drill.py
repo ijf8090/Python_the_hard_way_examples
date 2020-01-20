fa220a185019f5ea77e59ba86a530621353db77e
ijf8090 <i.finlay@yahoo.com>
2020-01-20 14:09:58 -0500

    moved function definition

--- ex33_drill.py
+++ ex33_drill.py
@@ -1,15 +1,6 @@
 i = 0
 numbers = []
-
-
 initial_count_value = input("Enter the starting value: ")
-
-loop_function(initial_count_value,numbers)
-
-print("The numbers: ")
-for num in numbers:
-  print(num)
-
 
 def loop_function(counter,list_var):
     while counter < 6:
@@ -19,3 +10,9 @@ def loop_function(counter,list_var):
      counter += 1
      print("numbers now: " , list_var)
      print("At the bottom counter is %d" % counter)
+
+loop_function(initial_count_value,numbers)
+
+print("The numbers: ")
+for num in numbers:
+  print(num)
